1ab2b26ca87ed3673c634987e5571c3829fdb402
Rajen Shrestha <rajen.shrestha@outlook.com>
2021-01-24 19:39:18 +1100

    Added fab buttons on main page

--- duty-book/src/components/app-home/app-home.tsx
+++ duty-book/src/components/app-home/app-home.tsx
@@ -22,6 +22,24 @@ export class AppHome {
       <ion-content class="ion-padding">
         <h1>Home Town</h1>
       </ion-content>,
+
+      <ion-content>
+        <ion-fab vertical="bottom" horizontal="end">
+          <ion-fab-button>Add</ion-fab-button>
+          <ion-fab-list side="top">
+            <ion-fab-button  color="danger">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </ion-fab-button>
+            <ion-fab-button color="success">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </ion-fab-button>
+            <ion-fab-button color="primary">
+              <ion-icon name="logo-youtube"></ion-icon>
+            </ion-fab-button>
+          </ion-fab-list>
+
+        </ion-fab>
+      </ion-content>
     ];
   }
 }
